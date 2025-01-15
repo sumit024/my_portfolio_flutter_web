1daee8ba5b2e4caed38c883e24270762097a65c7
sumitkushwaha <kushwahasumit024@gmail.com>
2025-01-15 21:58:24 +0530

    setting up the project revamp

---
+++ android/app/src/main/kotlin/com/wm/intro_web/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.wm.sumit_portfolio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:sumit_portfolio/presentation/first_page.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      minTextAdapt: true,
+      designSize: const Size(360, 690),
+      builder: (_,child){
+        return MediaQuery(
+          data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
+          child: MaterialApp(
+            title: 'Sumit Kushwaha',
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              primarySwatch: Colors.blue,
+            ),
+            home: const FirstPage(),
+          ),
+        );
+      },
+    );
+  }
+}
+
